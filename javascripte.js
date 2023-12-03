1c55d94b00e41d9eb7aa9feb3c26d6acaf0b8b03
rabiilfarakh <rabiilfarakh2816@gmail.com>
2023-12-03 13:22:01 +0100

    add modification (rabii)

--- javascripte.js
+++ javascripte.js
@@ -29,37 +29,7 @@ function validateConnForm() {
     return isValid;
 }
 
-// ------------------------------------------------------Validation_Inscription-----------------------------------------
-// function validateAndSubmitInscForm() {
-//     if (validateInscForm()) {
-//         openRoleSelectionModal();
-//         // Cacher la popup d'inscription
-//         closeInscriptionModal();
-//     }
-// }
-
-// function validateInscForm() {
-//     var form = document.getElementById('inscriptionForm2');
-//     var inputs = form.getElementsByTagName('input');
-//     var isValid = true;
-
-//     for (var i = 0; i < inputs.length; i++) {
-//         if (inputs[i].type === 'text' || inputs[i].type === 'password' || inputs[i].type === 'email') {
-//             if (inputs[i].value.trim() === '') {
-//                 inputs[i].style.border = '1px solid red';
-//                 isValid = false;
-//             } else {
-//                 inputs[i].style.border = ''; 
-//             }
-//         }
-//     }
-
-//     return isValid;
-// }
-
-
-{/* <nav class="navbar navbar-expand-lg bg-body-tertiary" style="border:1px solid white; border-radius:20px; width:40%;">
-<div class="container-fluid "style="dispaly:flex; gap:1.3vw"> */}
+// ___________________________________________________________________________________________
 
 
 
