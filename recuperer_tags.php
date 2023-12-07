ee2c723aa17838440bc444c505efda52e0eb7778
Douaa1819 <douaachemnane@gmail.com>
2023-12-07 09:50:14 +0100

    ne pas fobnctionne

--- recuperer_tags.php
+++ recuperer_tags.php
@@ -5,11 +5,17 @@ if (isset($_POST['idTheme'])) {
     $idTheme = $_POST['idTheme'];
 
     // Récupérer les tags associés au thème
-    $tagsQuery = $conn->query("SELECT t.nomTag FROM tags_theme tt JOIN tags t ON tt.idTag = t.idTag WHERE tt.idTh = '$idTheme'");
+    $query = "SELECT t.nomTag FROM tags_theme tt
+              JOIN tags t ON tt.idTag = t.idTag
+              WHERE tt.idTh = '$idTheme'";
+    $result = $conn->query($query);
 
-    while ($tag = $tagsQuery->fetch_assoc()) {
-        echo "<span>{$tag['nomTag']}</span>";
+    $tags = [];
+    while ($row = $result->fetch_assoc()) {
+        $tags[] = $row['nomTag'];
     }
+
+    // Afficher les tags sous forme de chaîne séparée par des virgules
+    echo implode(', ', $tags);
 }
 ?>
-
