2a78b9a54afdee86d861fa6b5534b90c8c245a97
anouar <anouar.ab95@gmail.com>
2023-12-12 11:01:44 +0100

    push

--- article.php
+++ article.php
@@ -434,7 +434,7 @@ if(isset($_POST['addComm'])){
             </button>
       </div>
       <div class="read d-flex justify-content-center align-items-center">
-        <button type="submit" name="read_more" id="read" class="btn btn-light-blue btn-md">Read more </button>
+        <a  href="./oneArticle.php?id=<?php echo $articleId?>" name="read_more" id="read" class="btn btn-light-blue btn-md">Read more </a>
         <svg xmlns="http://www.w3.org/2000/svg" style="cursor: pointer;" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
         <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8"/>
         </svg>
